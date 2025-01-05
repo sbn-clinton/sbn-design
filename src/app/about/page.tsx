8fa98a74ce0e5c8f18f9e12859ed3894f71740b9
Nwali Somto <somtobernardnwali619@gmail.com>
2025-01-05 13:58:04 +0100

    fouth commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AboutPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center bg-[#F8FAFF] text-center max-w-6xl mx-auto gap-y-5 md:gap-y-10 py-10 font-['Poppins']">
+      <div className="flex flex-col items-center justify-center bg-[#F8FAFF] text-center max-w-6xl mx-auto gap-y-5 md:gap-y-10 py-10 font-sans">
         <div className="flex flex-col items-center justify-center max-w-4xl mx-auto py-3">
           <Image
             src="/images/profile.png"
@@ -15,7 +15,7 @@ const AboutPage = () => {
           <h1 className="text-2xl md:text-5xl font-bold text-[212121] ">
             UX_ASIM
           </h1>
-          <p className="text-xl md:text-3xl font-bold text-[212121] ">
+          <p className="text-lg md:text-2xl font-bold text-[212121] ">
             UI/UX & Product Designer
           </p>
         </div>
@@ -32,46 +32,54 @@ const AboutPage = () => {
             👇👇👇
           </p>
         </div>
-        <div className="flex flex-row items-center gap-3 md:gap-7 w-[111px Hug] h-[48px Hug] justify-center px-4">
+        <div className="flex flex-row flex-wrap items-center gap-3 md:gap-7 max-w-4xl mx-auto justify-center px-4">
           <div className="flex flex-row items-center gap-2">
             <Image
               src="/images/link.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE] md:text-2xl  font-bold ">LinkedIn</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl  font-bold ">
+              LinkedIn
+            </p>
           </div>
           <div className="flex flex-row items-center gap-2 ">
             <Image
               src="/images/instagram.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE]  md:text-2xl font-bold">Instagram</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl font-bold">
+              Instagram
+            </p>
           </div>
           <div className="flex flex-row items-center gap-2">
             <Image
               src="/images/behance.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE]  md:text-2xl font-bold">Behance</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl font-bold">
+              Behance
+            </p>
           </div>
           <div className="flex flex-row items-center gap-2">
             <Image
               src="/images/dribble.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE]  md:text-2xl font-bold">Dribble</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl font-bold">
+              Dribble
+            </p>
           </div>
         </div>
         <div className="flex flex-row items-center gap-3 md:gap-7 w-[111px Hug] h-[48px Hug] justify-center px-4">
@@ -79,35 +87,39 @@ const AboutPage = () => {
             <Image
               src="/images/fiver.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE] md:text-2xl font-bold">Fiverr</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl font-bold">
+              Fiverr
+            </p>
           </div>
           <div className="flex flex-row items-center gap-2 ">
             <Image
               src="/images/upwork.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE]  md:text-2xl font-bold">Upwork</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl font-bold">
+              Upwork
+            </p>
           </div>
           <div className="flex flex-row items-center gap-2">
             <Image
               src="/images/x.png"
               alt="logo"
-              width={48}
-              height={48}
+              width={50}
+              height={50}
               className="fill-[#00189F] fill-opacity-[100%] w-[28px] h-[28px] md:w-[48px] md:h-[48px]"
             />
-            <p className="text-[#304FFE]  md:text-2xl font-bold">Xing</p>
+            <p className="text-[#304FFE] text-sm md:text-2xl font-bold">Xing</p>
           </div>
         </div>
 
-        <p className="text-[#00189F] md:text-2xl font-bold text-wrap px-5">
+        <p className="text-[#00189F] text-sm md:text-2xl font-bold text-wrap px-5">
           Design samples & get previews for inspiration and personal use only.
           Don&apos;t selling, reselling & commercial use it without permission.
           You can get more information about this design on my{" "}
@@ -122,11 +134,11 @@ const AboutPage = () => {
           <h1 className="text-[#00189F] text-xl md:text-4xl font-bold ">
             Need Custom Design
           </h1>
-          <p className="text-[#00189F] md:text-2xl font-bold text-wrap">
+          <p className="text-[#00189F] text-sm md:text-2xl font-bold text-wrap">
             Please Contact With My Email 👇👇👇
           </p>
         </div>
-        <p className="text-[#00189F] md:text-2xl font-bold text-wrap px-5 underline">
+        <p className="text-[#00189F] text-sm md:text-2xl font-bold text-wrap px-5 underline">
           <Link href="/" className="text-[#EB5757]">
             ux.asimbaig@gmail.com
           </Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,12 @@ const Navbar = () => {
     <div className=" bg-inherit backdrop-blur-md bg-opacity-25 fixed top-0 left-0 right-0 z-50 font-sans">
       <nav className="flex items-center justify-between max-w-6xl mx-auto p-4 md:py-5  ">
         <div className="flex items-center justify-center">
-          <h1 className="font-extrabold md:text-2xl text-lg text-[#F96C3B]">
+          <Link
+            href={"/"}
+            className="font-extrabold md:text-2xl text-lg text-[#F96C3B]"
+          >
             SBN
-          </h1>
+          </Link>
         </div>
         <div className="hidden md:flex">
           <ul className="flex items-center justify-center gap-x-4 md:gap-x-6 text-[#A6A6A6] text-lg font-medium">
